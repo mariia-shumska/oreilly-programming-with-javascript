cc4dd49db832776eecf1050910b4bc3cdaa00506
Mariia <lmarijka.v@gmail.com>
2017-03-23 12:58:15 +0200

    10_Functions-Summary_&_Practice

--- 02-functions/notes.js
+++ 02-functions/notes.js
@@ -6,21 +6,22 @@ console.log("this is another statement");
 //=> "Hello, Semmy!";
 
 var greet = function(name){
-  return "Hello, " + name
-}
+  return "Hello, " + name;
+};
 
 var greet1 = greet("Semmy");
 var greet2 = greet("Gracie");
-console.log(greet1)
-console.log(greet2)
+console.log(greet1);
+console.log(greet2);
 
 var toCardString = function(rank, suit){
   return rank + " of " + suit;
-}
+};
 
-console.log(toCardString("ace", "spades"));
 console.log(toCardString("two", "diamonds"));
 console.log(toCardString("queen", "hearts"));
+var aceOfSpades = toCardString("ace", "spades");
+console.log(aceOfSpades);
 
 // makeHtmlParagraph ("hello wrld");
 //=> <p>hello world</p>
@@ -28,8 +29,18 @@ var makeHtmlParagraph = function(content){
   var openParagraphTag = "<p>";
   var closeParagraphTag = "</p>";
   var htmlParagraph = openParagraphTag + content + closeParagraphTag;
-  return htmlParagraph
-}
+  return htmlParagraph;
+};
 
 var openParagraphTag = "This is not a tag";
 console.log(openParagraphTag);
+
+var add = function (x, y) {
+    var x = 100;  // we lose the argument x
+    console.log(x);
+    return x + y;
+};
+
+var addThree = function(first, second,  third){
+  return first+second+third;
+};
