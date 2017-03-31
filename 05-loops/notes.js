e58f1018b81f46200e7d1b4387bc6bfe574f1d77
Mariia <lmarijka.v@gmail.com>
2017-03-31 13:52:30 +0300

    25.Loops - while-loops and for-loopst

--- 05-loops/notes.js
+++ 05-loops/notes.js
@@ -1,2 +1,41 @@
 // your notes here!
 console.log("hello world!");
+
+console.log("these are the notes for section 5");
+
+var number = 0;
+
+while ( number<= 10){
+  if (number % 2 === 0) {
+      console.log("the  number is even");
+  }else {
+      console.log("the number is odd!");
+  }
+  number = number +1;
+}
+
+for (number = 0; number<=10; number = number +1){
+  if (number % 2 === 0) {
+      console.log("the  number is even");
+  }else {
+      console.log("the number is odd!");
+  }
+  }
+
+
+var count;
+for (count = 0; count<=100; count = count + 1){
+  console.log("the number is: " + count)
+}
+
+var num;
+for (num = 0; num<100; num = num+1){
+  if (num % 3 === 0){
+      console.log(num + " is multiple of 3 and smaller than 100");
+}
+}
+
+var num2;
+for (num2 = 0; num2<100; num2 = num2+3){
+  console.log("this multiple of three is: " + num2)
+}
