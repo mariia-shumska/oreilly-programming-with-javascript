63993aee41aa7139da4e45ed8f2f4b20010c3bcd
Mariia <lmarijka.v@gmail.com>
2017-04-01 14:48:38 +0300

    32.Arrays - Similarities to Strings

---
+++ 06-arrays/32_Arrays-Similarities_to_Strings.js
@@ -0,0 +1,56 @@
+var greeting = "hello world";
+undefined
+greeting.charAt(3)
+"l"
+greeting.charAt[3]
+undefined
+var list = [1,2,3,4,5]
+undefined
+list[3]
+4
+greeting
+"hello world"
+greeting(3)
+greeting.charAt(3)
+"l"
+greeting.charAt(greeting.length -1)
+"d"
+list[list.length -1]
+5
+greeting[greeting.length - 1]
+"d"
+var tweet = "this is a tweet" + "! lol!"
+undefined
+tweet
+"this is a tweet! lol!"
+
+var numbers = [1,2,3,4].concat[5,6,7,8]
+undefined
+numbers
+undefined
+var numbers = [1,2,3,4].concat([5,6,7,8])
+undefined
+numbers
+[1, 2, 3, 4, 5, 6, 7, 8]
+
+var tweets = ["this is a tweet", "awsome", "lol"];
+undefined
+tweets
+["this is a tweet", "awsome", "lol"]
+tweets.indexOf("lol")
+2
+tweets.indexOf("omg")
+-1
+numbers
+[1, 2, 3, 4, 5, 6, 7, 8]
+
+var numbersString = "12345678";
+undefined
+numbers.slice(3)
+[4, 5, 6, 7, 8]
+numbersString.slice(3)
+"45678"
+numbers.slice(3,5)
+[4, 5]
+numbersString.slice(3,5)
+"45"
